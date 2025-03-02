f685a9f5dae3e35f434b27229b426a780e087304
AdeshOak <adeshoak22@gmail.com>
2025-03-01 17:49:15 -0800

    Fixed refresh error, code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+//Import statements
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
